a73142f4278fde3b502a18653a7075864e7d8024
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2026-02-06 00:08:56 +0900

    account_setting OAuth

--- views/account_setting.php
+++ views/account_setting.php
@@ -5,14 +5,11 @@ include("../functions.php");
 check_session_id();
 
 // サインインユーザー情報を取得
-$uName = $_SESSION['user']['uName'];
-$uMail = $_SESSION['user']['uMail'];
-
-
-$success = $_SESSION['success'] ?? null;
-
-// 取得したセッションのクリア
-unset($_SESSION['success']);
+$firebase_uid = $_SESSION["firebase_uid"];
+$email = $_SESSION["email"];
+$display_name = $_SESSION["display_name"];
+$photo_url = $_SESSION["photo_url"];
+$provider = $_SESSION["provider"];
 
 include("../views/header.php");
 ?>
@@ -20,23 +17,20 @@ include("../views/header.php");
 <div class="account-setting-container">
     <h2 class="account-setting-title">アカウント設定</h2>
     <!-- サクセスメッセージが登録されていれば表示 -->
-    <?php if (!empty($success)): ?>
-        <p>
-            <span class="success-msg"><?= $success ?></span>
-        </p>
-    <?php endif; ?>
+    <span id="success-msg" class="success-msg" style="display: none;"></span>
     <a href="../views/home.php">ホーム画面へ</a>
-    <form class="account-setting-form" action="../controllers/account_update.php" method="post">
+    <form class="account-setting-form">
         <div>
-            <input type="text" name="uName" id="uName" placeholder="ユーザー名" readonly value="<?= $uName ?>">
+            <input id="displayName" name="displayName" placeholder="ユーザー名" readonly value="<?= $display_name ?>">
         </div>
-        <div>
-            <input type="email" name="uMail" id="uMail" placeholder="メールアドレス" readonly value="<?= $uMail ?>">
-        </div>
-        <a href="./account_update.php" class="account-setting-btn" style="color:black;background-color:#f0f0f0; text-decoration: none;">編集</a>
+        <a href="../views/account_update.php" class="account-setting-btn" style="color:black;background-color:#f0f0f0; text-decoration: none;">編集</a>
     </form>
     <a href="../views/account_delete.php" style="color: red;">アカウントを削除</a>
 </div>
+
+<!-- jQuery読み込み -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script type="module" src="../assets/js/firebase.js"></script>
 </body>
 
 </html>
