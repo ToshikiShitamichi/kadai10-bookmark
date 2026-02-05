c4019455aa923d094d972ff4c5842b5c6ee6b33c
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2026-02-06 00:09:07 +0900

    account_delete OAuth

--- controllers/account_delete.php
+++ controllers/account_delete.php
@@ -4,12 +4,6 @@ session_start();
 include("../models/User.php");
 $userModel = new User();
 
-// サインインユーザー情報を取得
-$uId = $_SESSION['user']['uId'];
-
-// SQL実行
-$userModel->deleteUser($uId);
-
 $_SESSION = array();
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time() - 42000, '/');
